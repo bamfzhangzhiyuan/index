93517672f74363f233f634babcc58b344ecb161f
zhangzhiyuan <1157390260@qq.com>
2019-01-07 08:29:07 +0800

    完成第四屏

--- new.js
+++ new.js
@@ -12,6 +12,9 @@ window.onload=function () {
 
     var liNodes=document.querySelectorAll(".list li")
     var firstLi=liNodes[0].querySelector(".down");
+    var firstPoint=document.querySelectorAll(".firstPoint li")
+    var firstScreen=document.querySelectorAll(".firstScreen li")
+    var firstwrap=document.querySelector(".contentMain>li:nth-child(1) .liMain")
 
    init()
 
@@ -62,7 +65,7 @@ window.onload=function () {
            child.style.top=-con.offsetHeight*sum+"px"
            move(sum)
        }
-
+       contentmove(3)
        /*兼容滚轮事件*/
        function wheel(event) {
            clearTimeout(timr)
@@ -113,4 +116,64 @@ window.onload=function () {
 
    }
 
+   First()
+   function First() {
+
+        var oldindex=0
+        var newindex=0
+        var nowtime=0
+        var lasttime=0
+        var timer=null
+       for (var i = 0; i <firstPoint.length; i++) {
+
+           firstPoint[i].index=i
+           firstPoint[i].onclick=function () {
+               newindex=this.index
+               nowtime=Date.now()
+
+               if(nowtime-lasttime<=2000) return
+
+               if (newindex===oldindex) return
+
+               if (newindex>oldindex){
+                   firstScreen[newindex].className="common rightshow"
+                   firstScreen[oldindex].className="common lefthide"
+               }else {
+                   firstScreen[newindex].className="common leftshow"
+                   firstScreen[oldindex].className="common righthide"
+               }
+               firstPoint[newindex].className="active"
+               firstPoint[oldindex].className=""
+
+              oldindex=newindex
+               lasttime=nowtime
+
+
+           }
+       }
+       firstwrap.onmouseenter=function () {
+          clearInterval(timer)
+       }
+       firstwrap.onmouseleave=function () {
+           auto()
+       }
+       auto()
+       function auto() {
+           timer=setInterval(function () {
+               newindex++
+               lasttime=Date.now()
+               if(newindex==4){
+                   newindex=0
+               }
+               firstScreen[newindex].className="common rightshow"
+               firstScreen[oldindex].className="common lefthide"
+
+               firstPoint[newindex].className="active"
+               firstPoint[oldindex].className=""
+               oldindex=newindex
+           },3000)
+       }
+
+
+   }
 }
